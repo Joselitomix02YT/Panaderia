574a7e72b4a9a05148943d2e266d657023394ff4
DanielPerezVapo <angeldanielperezarce2008@gmail.com>
2025-11-21 14:44:02 -0600

    map

--- public/scripts/mapa/map.js
+++ public/scripts/mapa/map.js
@@ -1,4 +1,4 @@
-const map = L.map('map').setView([51.505, -0.09], 13);
+const map = L.map('map').setView([19.432672, -99.133062], 13);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
